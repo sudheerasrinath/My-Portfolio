dbe4c8f8c317b1e4b8d3d542f5e0b4486a065e7b
sudheerasrinath <sudheerasrinath99@gmail.com>
2022-11-27 22:50:51 +0530

    js add

---
+++ assignments/JS/Game/script.js
@@ -0,0 +1,7 @@
+const score=document.querySelector('.score');
+const startScreen=document.querySelector('.startScreen');
+const gameArea=document.querySelector('.gameArea');
+/*console.log(gameArea);*/
+startScreen.addEventListener('click',start);
+let player={speed:5,score:0};
+let keys ={ArrowUp:false,ArrowDown:false,ArrowLeft:false,ArrowRight:false}
